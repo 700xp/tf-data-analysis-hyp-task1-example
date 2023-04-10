181f35b8f9322fa3296692c25ac8671f41437722
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-10 23:58:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,6 +12,6 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     _,p_val = proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'smaller')
-    if(p_val < sgn_lvl):
+    if(p_val > sgn_lvl):
         return False
     return True 
