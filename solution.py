588f1b6b950d28a796e3e6eafb47f6ac7dcbd098
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-10 22:38:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,12 +5,13 @@ from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 689327667 # Ваш chat ID, не меняйте название переменной
 
+sgn_lvl = 0.01
+
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
     _,p_val = proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'smaller')
-    sgn_lvl = 0.01
     if(p_val < sgn_lvl):
         return False
     return True 
